d5f99db18b6734d71601b2707a50f758e963e157
thamenato <thamenato@gmail.com>
2014-10-02 15:38:07 -0300

    layout para PEP8

--- t2-random-walks.py
+++ t2-random-walks.py
@@ -1,5 +1,9 @@
-""" Trabalho T2 da disciplina Teoria dos Grafos, ministrada em 2014/02
-	Thales, Daniel e Jorge
+"""
+Trabalho T2 da disciplina Teoria dos Grafos, ministrada em 2014/02
+    Alunos:
+        Daniel  Nobusada
+        Thales Eduardo Adair Menato         407976
+        Jorge Augusto Bernardo              407844
 """
 import networkx as nx
 import numpy as np
@@ -13,25 +17,26 @@ for i in G.edge:
         w_real.append(aux)
 
 a_m = nx.adjacency_matrix(G)
-sum_a_m = []  
+sum_a_m = []
 for i in a_m:
-  sum_a_m.append(i.sum())
+    sum_a_m.append(i.sum())
 
 p_aux_matrix = []
-for i in range(0,_NUM_NODES):
-  line_array = a_m[0].getA1()
-  line = []
-  for j in line_array:
-    value = j / sum_a_m[i]
-    line.append(value)
-  p_aux_matrix.append(line)
+for i in range(0, _NUM_NODES):
+    line_array = a_m[0].getA1()
+    line = []
+
+    for j in line_array:
+        value = j / sum_a_m[i]
+        line.append(value)
+
+    p_aux_matrix.append(line)
 p_matrix = np.matrix(p_aux_matrix)
 
 w_power5 = np.multiply(p_matrix, p_matrix)
-for i in range(0,3):
-  w_power5 = np.multiply(w_power5, p_matrix)
+for i in range(0, 3):
+    w_power5 = np.multiply(w_power5, p_matrix)
 
 w_power100 = np.multiply(p_matrix, p_matrix)
 for i in range(0, 98):
-  w_power100 = np.multiply(w_power100, p_matrix)
-
+    w_power100 = np.multiply(w_power100, p_matrix)
